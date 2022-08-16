6aa12ea20026e00c9893f754b36362712dff202e
odi1n <sashf1999@mail.ru>
2022-08-17 01:30:00 +0300

    Значение kinopoisk_id может быть None

--- videocdn_tv/models/api/movie.py
+++ videocdn_tv/models/api/movie.py
@@ -10,7 +10,7 @@ class Movie(BaseModel):
     ru_title: str
     orig_title: str
     imdb_id: str = None
-    kinopoisk_id: int
+    kinopoisk_id: int = None
     default_media_id: str = None
     created: str
     released: str
