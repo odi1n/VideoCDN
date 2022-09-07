6de5b7227305642ae8205951cfdec875f55cd5b2
odi1n <sashf1999@mail.ru>
2022-09-07 14:49:50 +0300

    Вынес одинаковые параметры в отдельный класс

--- videocdn_tv/models/api/anime_tv_series.py
+++ videocdn_tv/models/api/anime_tv_series.py
@@ -1,29 +1,16 @@
 from typing import List
 
-from pydantic import BaseModel
+from videocdn_tv.models.api.base_api import BaseApi
+from videocdn_tv.models.contents import Episode
 
-from videocdn_tv.models.contents import Translation, Episode
 
-
-class AnimeTvSeries(BaseModel):
-    id: int
-    ru_title: str
-    orig_title: str
+class AnimeTvSeries(BaseApi):
     en_title: str
     other_title: str
-    imdb_id: str
-    kinopoisk_id: str
     worldart_id: str
     season_count: int
     episode_count: int
     last_episode_id: int
     start_date: str
     end_date: str = None
-    created: str
-    updated: str
-    blocked: int
-    preview_iframe_src: str
-    iframe_src: str
-    iframe: str
-    translations: List[Translation]
     episodes: List[Episode]
--- videocdn_tv/models/api/show_tv_series.py
+++ videocdn_tv/models/api/show_tv_series.py
@@ -1,26 +1,13 @@
 from typing import List
 
-from pydantic import BaseModel
+from videocdn_tv.models.api.base_api import BaseApi
+from videocdn_tv.models.contents import Episode
 
-from videocdn_tv.models.contents import Translation, Episode
 
-
-class ShowTvSeries(BaseModel):
-    id: int
-    ru_title: str
-    orig_title: str
-    imdb_id: str
-    kinopoisk_id: str
+class ShowTvSeries(BaseApi):
     season_count: int
     episode_count: int
     last_episode_id: int
     start_date: str
     end_date: str = None
-    created: str
-    blocked: int
-    updated: str
-    preview_iframe_src: str
-    iframe_src: str
-    iframe: str
-    translations: List[Translation]
     episodes: List[Episode]
