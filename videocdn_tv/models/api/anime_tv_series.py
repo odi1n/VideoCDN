6a31c2d3e7be5c0ce13f0f056683853e0e6c743a
odi1n <sashf1999@mail.ru>
2022-09-07 14:56:43 +0300

    Поправил параметры, изменил по умолчанию данные

--- videocdn_tv/models/api/anime_tv_series.py
+++ videocdn_tv/models/api/anime_tv_series.py
@@ -6,8 +6,8 @@ from videocdn_tv.models.contents import Episode
 
 class AnimeTvSeries(BaseApi):
     en_title: str
-    other_title: str
-    worldart_id: str
+    other_title: str = None
+    worldart_id: str = None
     season_count: int
     episode_count: int
     last_episode_id: int
