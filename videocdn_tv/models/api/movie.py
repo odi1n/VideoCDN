aa50e4ac03a6a54db6a0bd1534cddae8d47e402e
odi1n <sashf1999@mail.ru>
2022-09-07 14:50:07 +0300

    Вынес одинаковые параметры в отдельный класс

--- videocdn_tv/models/api/movie.py
+++ videocdn_tv/models/api/movie.py
@@ -1,25 +1,12 @@
 from typing import List
 
-from pydantic import BaseModel
+from videocdn_tv.models.api.base_api import BaseApi
+from videocdn_tv.models.contents import Media
 
-from videocdn_tv.models.contents import Media, Translation
 
-
-class Movie(BaseModel):
-    id: int
-    ru_title: str
-    orig_title: str
-    imdb_id: str = None
-    kinopoisk_id: int = None
+class Movie(BaseApi):
     default_media_id: str = None
-    created: str
     released: str
-    updated: str = None
-    blocked: int
     media: List[Media]
-    preview_iframe_src: str
-    iframe_src: str
-    iframe: str
-    translations: List[Translation] = None
     year: str
     content_type: str
